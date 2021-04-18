eaf20e26f982408fc5df7a8866116d1295ffb8ab
Suman Barick <codotronix@gmail.com>
2021-04-18 14:04:14 +0530

    landing page wip

--- fe/src/app/app.module.ts
+++ fe/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatInputModule } from '@angular/material/input';
+import { MatIconModule } from '@angular/material/icon';
+import { HeaderComponent, FooterComponent } from './components/layout';
+import { LandingComponent } from './components/pages';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LandingComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ fe/src/app/components/pages/index.ts
@@ -0,0 +1,5 @@
+import { LandingComponent } from './landing/landing.component'
+
+export {
+    LandingComponent
+}
