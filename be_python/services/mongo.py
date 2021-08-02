db7c07c4439fcf52c7a802c44d6c997a8ccb0234
Suman Barick <codotronix@gmail.com>
2021-08-02 21:53:45 +0530

    api return objects decorated with status and msg

--- be_python/services/mongo.py
+++ be_python/services/mongo.py
@@ -16,29 +16,47 @@ def signup(user_data):
     # if already signed up
     # ask to login
     if find_user(user_data["email"]):
-        return 'This email already exists. Please proceed to login.'
+        return {
+            "status": 400,
+            "msg": "This email already exists. Please proceed to login."
+        }
     
     # if email does not exist in db
     db = client.parikshak_db
     collection = db.user_collection_1
     user_data["pswd"] = hash_it(user_data["pswd"])
     user_id = collection.insert_one(user_data).inserted_id
-    return str(user_id)
+    return {
+        "status": 200,
+        "msg": "Signup successful.",
+        "userId": str(user_id)
+    }
 
 
 def signin(email, pswd):
     # see if user with this email exists
     user = find_user(email)
     if not user:
-        return 'This email is not registered with us. Please signup first.'
+        return {
+            "status": 400,
+            "msg": "This email is not registered with us. Please signup first."
+        }
     
     # if user exists
     if not verify_hash(pswd, user["pswd"]):
-        return 'The email / password combination did not match. Please try again.'
+        return {
+            "status": 400,
+            "msg": "The email / password combination did not match. Please try again."
+        }
 
-    # return a jwt token
+    # return 
+    del user["pswd"]
+    del user["_id"]
     return { 
         "tok": jwt_encode({ 
             "email": email 
-        }) 
+        }),
+        "user": user,
+        "status": 200,
+        "msg": "Signin successful."
     }
