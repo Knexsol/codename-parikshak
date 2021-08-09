a7505c1dcac6f106e75aae07635eef13222985c3
Suman Barick <codotronix@gmail.com>
2021-08-09 14:11:40 +0530

    category and subcategory wip

--- fe/src/app/components/pages/create-exam/create-exam-intro-section/create-exam-intro-section.component.ts
+++ fe/src/app/components/pages/create-exam/create-exam-intro-section/create-exam-intro-section.component.ts
@@ -1,5 +1,12 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { MatSelectChange } from '@angular/material/select';
 import { TQPaperIntro } from 'src/app/types/question.types';
+
+type TCat = {
+  name: string,
+  value: string
+  subcat?: Array<TCat>
+}
 
 @Component({
   selector: 'app-create-exam-intro-section',
@@ -9,9 +16,41 @@ import { TQPaperIntro } from 'src/app/types/question.types';
 export class CreateExamIntroSectionComponent implements OnInit {
   @Input() qaIntroObj!: TQPaperIntro
 
+  cats: Array<TCat> = [
+    { 
+      name: 'Engineering', 
+      value: 'engineering',
+      subcat: [
+        { name: 'Electronics and Communication', value: 'ece' },
+        { name: 'Electrical', value: 'ee' },
+        { name: 'Chemical', value: 'ch' },
+      ]
+    },
+    { 
+      name: 'IT', 
+      value: 'it',
+      subcat: [
+        { name: 'Networking', value: 'ntwk' },
+        { name: 'Cloud Computing', value: 'cc' },
+        { name: 'Web Dev', value: 'wd' },
+      ]
+    },
+  ]
+
+  subcats: Array<TCat> = []
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onCatSelect (ev: MatSelectChange) {
+    // console.log('Selected id = ', ev)
+    for(let c of this.cats) {
+      if(c.value === ev.value) {
+        this.subcats = c.subcat!
+      }
+    }
   }
 
 }
