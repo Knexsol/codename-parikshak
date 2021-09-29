6360347dd9990059f7f85f540e58ec75de608e50
Suman Barick <codotronix@gmail.com>
2021-09-30 02:24:31 +0530

    gapi zone issue fixed with proper promise

--- fe/src/app/services/google-sign-in.service.ts
+++ fe/src/app/services/google-sign-in.service.ts
@@ -1,46 +1,72 @@
 import { Injectable } from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class GoogleSignInService {
-  GoogleAuth: any
+    GoogleAuth: any
 
-  initComplete = false
+    initComplete = false
 
-  constructor() { }
+    constructor() { }
 
-  initLoad () {
-    if(!this.initComplete) {
-      // @ts-ignore
-      if(typeof(gapi) !== 'undefined') {
-        this._initGapi()
-        this.initComplete = true
-      }
-      else {
-        setTimeout(() => {
-          this.initLoad()
-        }, 1000)
-      }
+    initLoad() {
+        return new Promise( (resolve: Function) => {
+            if (!this.initComplete) {
+                // @ts-ignore
+                // if(typeof(gapi) !== 'undefined') {
+                this._waitForGapi().then(() => {
+                    this._initGapi().then(() => resolve('Google Signin API Ready to Rock...'))
+                })
+                this.initComplete = true
+            }
+        } )
     }
-    
-  }
 
-  isSignedIn = ():boolean => this.GoogleAuth.isSignedIn.get()
+    /**
+     * Let's wait for the Global 'gapi' to be loaded on windows
+     * @returns 
+     */
+    _waitForGapi() {
+        return new Promise((resolve: Function) => {
+            // let isGapiAvailable = false
+            function resolveIfGapiAvailable() {
+                // @ts-ignore
+                if (typeof (gapi) !== 'undefined') {
+                    resolve()
+                }
+                else {
+                    setTimeout(() => {
+                        resolveIfGapiAvailable()
+                    }, 1000)
+                }
+            }
+            resolveIfGapiAvailable()
+        })
+    }
 
-  signIn
-  signOut = () => this.GoogleAuth.signOut()
+    isSignedIn = (): boolean => this.GoogleAuth.isSignedIn.get()
 
-  _initGapi() {
-    // @ts-ignore
-    gapi.load('auth2', () => {
-      /* Ready. Make a call to gapi.auth2.init or some other API */
-      // @ts-ignore
-      this.GoogleAuth = gapi.auth2.init({
-        client_id: '823183251448-2h0kt0h3druin14f3fmpav8qkscs1o46.apps.googleusercontent.com'
-      })
 
-      console.log(this.GoogleAuth)
-    });
-  }
+    signOut = () => this.GoogleAuth.signOut()
+
+    /**
+     * Initialize gapi with 'auth2'
+     * @returns 
+     */
+    _initGapi() {
+        return new Promise( (resolve:Function) => {
+            // @ts-ignore
+            gapi.load('auth2', () => {
+                /* Ready. Make a call to gapi.auth2.init or some other API */
+                // @ts-ignore
+                this.GoogleAuth = gapi.auth2.init({
+                    client_id: '823183251448-2h0kt0h3druin14f3fmpav8qkscs1o46.apps.googleusercontent.com'
+                })
+                resolve('gapi load auth2 -- complete')
+                console.log(this.GoogleAuth)
+            });
+        } )
+        
+    }
 }
